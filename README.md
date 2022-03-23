4c1b389eec3be60e5e448754e7a5e9167f763ac5
Pirjot <apirjot@gmail.com>
2022-03-22 23:31:03 -0700

    Main GUI framework

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # AuxBattle
-
 By Harjyot Sidhu, Pirjot Atwal, Harkamal Padda
+
+## Intro
+
+Welcome to AuxBattle, a website developed by a few college friends to
+emulate the popular game of Aux Battle online! 
+
+## How to Run
+To run the server locally on your computer, make sure to install Node JS (and npm),
+and the git terminal tool (from git-scm). 
+
+1. Clone the repo to your computer using:
+```
+git clone https://github.com/hsidhu6/auxbattle
+```
+2. Install node dependencies with:
+```
+npm install
+```
+3. Run the server using:
+```
+node app.js
+```
+4. Visit the website at the following URL:
+```
+http://localhost:3000
+```
+
+## Online Site
+This is under development! Check back here to see our official online deployment.
---
+++ app.js
@@ -0,0 +1,28 @@
+/**
+ * AuxBattle Server file
+ * 
+ * Sets up and initializes the server app, its dependencies and relative get/post routes.
+ * All related logic is handled in routes.js
+ * 
+ * @author Harjyot Sidhu, Pirjot Atwal, Harkamal Padda
+ */
+
+console.log("Welcome to AuxBattle! Setting up the server...");
+
+const express = require('express'); // Import the express module
+const app = express(); // Build the app
+
+app.use(express.static('public')); // By default, serve static files from the public folder
+app.use(express.json()); // Allow JSON GET/POST requests
+
+
+// Listen on port 3000 (localhost:3000)
+let listener = app.listen(process.env.PORT || 3000, () => {
+    console.log("Starting to listen at localhost:" + listener.address().port);
+});
+
+//On server/process closing, perform cleanup functions
+process.on('SIGINT', () => {
+    console.log("\nClosing down AuxBattle server...");
+    process.exit(0);
+});
